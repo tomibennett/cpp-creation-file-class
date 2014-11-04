4168020216c07bcbb6915815ecb26ba2289d83e7
loic lefloch <l.lefloch.91@gmail.com>
2014-11-04 17:29:14 +0100

    many changements

---
+++ src/test.cpp
@@ -0,0 +1,20 @@
+/*
+ * Test for project cpp-creation-file-class
+ *
+ * made by test
+ */
+
+#include "Test.hpp"
+
+//ctors & dtors
+Test::Test {}
+
+Test::Test(const Test& oth) {}
+
+Test& Test::operator=(const Test& rht) {}
+
+Test::~Test() {}
+
+
+// members functions
+
